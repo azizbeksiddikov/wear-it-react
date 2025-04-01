973065d1e1fb8444a9f55e7bff9f4f106607c8f1
Azizbek Siddikov <azbeksid@gmail.com>
2025-04-01 10:04:23 +0900

    fix: organize homePage realted sectional components

---
+++ src/app/screens/homePage/SaleClothes.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from '@mui/material';
+
+export default function SaleClothes() {
+	return (
+		<div>
+			<Container>SaleClothes</Container>
+		</div>
+	);
+}
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React from 'react';
+import Statistics from './Statistics';
+import FeatuedClothes from './FeatuedClothes';
+import SaleClothes from './SaleClothes';
+import Advertisement from './Advertisement';
+import Events from './Events';
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+	return (
+		<div className={'homepage'}>
+			<Statistics />
+			<FeatuedClothes />
+			<SaleClothes />
+			<Advertisement />
+			<Events />
+		</div>
+	);
 }
