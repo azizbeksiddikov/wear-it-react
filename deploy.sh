24c4b915b5845b678bd3f940699f7c80b9bb189d
Azizbek Siddikov <azbeksid@gmail.com>
2025-12-28 12:49:20 +0900

    fix: update to conditionally install some packages

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # PRODUCTION
-git reset --hard 
-git checkout master
-git pull origin master
+# git reset --hard 
+# git checkout master
+# git pull origin master
 
-npm i yarn -g
+if ! command -v yarn &> /dev/null; then
+    npm i yarn -g
+fi
+
+# Install serve if not present
 yarn global add serve
+
+if ! command -v pm2 &> /dev/null; then
+    yarn global add pm2
+fi
+
 yarn
 yarn run build
 pm2 start "yarn run start:prod" --name=WEAR-IT-REACT
-
